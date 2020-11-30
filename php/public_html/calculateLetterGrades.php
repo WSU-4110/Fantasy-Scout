d455df6b3d10c3eacda42a43a3c16e55314603e9
Joshua Perkey <ga1659@wayne.edu>
2020-11-29 20:56:34 -0500

    -fixed syntax error

--- php/public_html/calculateLetterGrades.php
+++ php/public_html/calculateLetterGrades.php
@@ -23,7 +23,7 @@ while ($org = mysqli_fetch_array($Organizations)) {
     $score = $org["ratingN"];
     $orgID = $org["orgID"];
     // If the score is less than the nextbestscore or greater than the nextworstscore, they have already been assigned an S or F, so skip
-    if ($score < $nextbestScore OR $score > $nextworstScore) {continue};
+    if ($score < $nextbestScore OR $score > $nextworstScore) {continue}
     // GRADE A
     if ($score >= $Aceil AND $score < $Bceil ) {
         // A+
