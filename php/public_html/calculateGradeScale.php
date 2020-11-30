29b9e97914f67aa52aabcecf1dff2ee1f6a3337a
Joshua Perkey <ga1659@wayne.edu>
2020-11-29 20:51:49 -0500

    -version1 of calculateLetterGrades.php complete -fixed some errors in calculateGradeScale.php

--- php/public_html/calculateGradeScale.php
+++ php/public_html/calculateGradeScale.php
@@ -14,15 +14,19 @@ $getAnalysts = "
     FROM Analysts
     ORDER BY ratingN ASC;
 ";
-$Organizations = mysqli_fetch_array(mysqli_query($con,$getOrgs));
-$Analysts = mysqli_fetch_array(mysqli_query($con,$getAnalysts));
+$Organizations = mysqli_query($con,$getOrgs);
+$Analysts = mysqli_query($con,$getAnalysts);
 
 // FIND BEST AND WORST SCORES
 // Find best/worst organization/analyst
-$bestOrg = $Organizations[0];
-$worstOrg = $Organizations[sizeof($Organizations)-1];
-$bestAnalyst = $Analysts[0];
-$worstAnalyst = $Analysts[sizeof($Analysts)-1];
+$bestOrg = mysqli_fetch_array($Organizations);
+while ($row = mysqli_fetch_array($Organizations)) {
+    $worstOrg = mysqli_fetch_array($Organizations);
+}
+$bestAnalyst = mysqli_fetch_array($Analysts);
+while ($row = mysqli_fetch_array($Analysts)) {
+    $worstAnalyst = mysqli_fetch_array($Analysts);
+}
 
 // From the best/worst org/analyst, determine which is the best and which is the worst
 // Find best
@@ -40,6 +44,7 @@ elseif ($bestOrg["ratingN"] < $bestAnalyst["ratingN"]) {
     $bestScore = $bestOrg["ratingN"];
     $bestTable = "Organizations";
     $bestID = $best["orgID"];
+    $bestIDtype = "orgID"
 }
 else {
     // The scores are the same
@@ -90,8 +95,50 @@ else {
 }
 
 // calculate A-E grading scale, and within those scales calculate +- scales
-
-
+// Find next best and next worst score
+$nextbestOrg = $Organizations[1];
+$nextworstOrg = $Organizations[sizeof($Organizations)-2];
+$nextbestAnalyst = $Analysts[1];
+$nextworstAnalyst = $Analysts[sizeof($Analysts)-2];
+if ($nextbestOrg["ratingN"] > $nextbestAnalyst["ratingN"]) {
+    // Next best analyst has best score
+    $nextbest = $nextbestAnalyst;
+    $nextbestScore = $nextbestAnalyst["ratingN"];
+    /*$bestTable = "Analysts";
+    $bestID = $best["analystID"];
+    $bestIDtype = "analystID";*/
+}
+elseif ($nextbestOrg["ratingN"] < $nextbestAnalyst["ratingN"]) {
+    // Next best organization has best score
+    $nextbest = $nextbestOrg;
+    $nextbestScore = $nextbestOrg["ratingN"];
+    /*$bestTable = "Organizations";
+    $bestID = $best["orgID"];*/
+}
+else {
+    // The scores are the same
+}
+if ($nextworstOrg["ratingN"] < $nextworstAnalyst["ratingN"]) {
+    // Next worst analyst has worst score
+    $nextworst = $nextworstAnalyst;
+    $nextworstScore = $nextworstAnalyst["ratingN"];
+    /*$worstTable = "Analysts";
+    $worstID = $worst["analystID"];
+    $worstIDtype = "analystID";*/
+}
+elseif ($nextworstOrg["ratingN"] > $nextworstAnalyst["ratingN"]) {
+    // Next worst organization has worst score
+    $nextworst = $nextworstOrg;
+    $nextworstScore = $nextworstOrg["ratingN"];
+    /*$worstTable = "Organizations";
+    $worstID = $worst["orgID"];*/
+}
+else {
+    // The scores are the same
+}
+// Calculate difference between next best and next worst score
+$range = $nextworstScore - $nextbestScore;
+$grade = $range/5;
 
 
 // ***CLOSE CONNECTION WITH SERVER***
